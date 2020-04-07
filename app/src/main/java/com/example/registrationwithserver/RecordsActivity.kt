2d98ec67955db1e56bc6f899766a76bc213b17d2
Ankit Tiwari (ak) <ankit2610akt@gmail.com>
2020-04-07 11:41:39 +0530

    feat: Add view binding

--- app/src/main/java/com/example/registrationwithserver/RecordsActivity.kt
+++ app/src/main/java/com/example/registrationwithserver/RecordsActivity.kt
@@ -4,30 +4,30 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.registrationwithserver.databinding.ActivityRecordsBinding
 import java.util.*
 
 class RecordsActivity : AppCompatActivity() {
-    var recordList: RecyclerView? = null
     var registeredDataArrayList: ArrayList<RegisteredData>? = null
     var recordAdapter: RecordAdapter? = null
+    private lateinit var binding: ActivityRecordsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_records)
+        binding = ActivityRecordsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         registeredDataArrayList = intent.getParcelableArrayListExtra("data")
         recordAdapter = RecordAdapter(this, registeredDataArrayList)
-        setViews()
         configureRecyclerView()
     }
 
     private fun configureRecyclerView() {
         val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(this, 1)
-        recordList!!.layoutManager = layoutManager
-        recordList!!.setHasFixedSize(true)
-        recordList!!.adapter = recordAdapter
+        binding.recordList.layoutManager = layoutManager
+        binding.recordList.setHasFixedSize(true)
+        binding.recordList.adapter = recordAdapter
         recordAdapter!!.notifyDataSetChanged()
     }
 
-    private fun setViews() {
-        recordList = findViewById(R.id.record_list)
-    }
 }
